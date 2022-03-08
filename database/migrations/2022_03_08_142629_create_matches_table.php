aef452bb5f292069c160957354af175b6dbdfc5c
AlVirtual <virtualweb2020@gmail.com>
2022-03-08 16:30:56 +0100

    creadas y ejecutadas las migraciones definitivas

---
+++ database/migrations/2022_03_08_142629_create_matches_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+
+            $table->dateTime('date');
+            
+            $table->unsignedBigInteger('local_team_id');
+            $table->foreign('local_team_id')->references('id')->on('teams')
+                    ->onUpdate('cascade')
+                    ->onDelete('no action');
+            
+            $table->unsignedBigInteger('visitor_team_id');
+            $table->foreign('visitor_team_id')->references('id')->on('teams')
+                    ->onUpdate('cascade')
+                    ->onDelete('no action');
+            
+            $table->tinyInteger('goals_local');
+            $table->tinyInteger('goals_visitor');
+            
+            $table->unsignedBigInteger('stadium_id');
+            $table->foreign('stadium_id')->references('id')->on('stadia')
+                    ->onUpdate('cascade')
+                    ->onDelete('no action');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
