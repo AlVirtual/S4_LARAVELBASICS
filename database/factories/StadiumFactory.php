9516c9422ed2fb4ad8e7ba54f1d721cc1924d1cc
AlVirtual <virtualweb2020@gmail.com>
2022-03-10 12:28:48 +0100

    pruebas con seeder y factories. se dejan para má adelante

--- database/factories/StadiumFactory.php
+++ database/factories/StadiumFactory.php
@@ -3,9 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Stadium;
 
 class StadiumFactory extends Factory
 {
+    
+    /**
+     * The name of the factory's corresponding model.
+     * 
+     * @var string
+     */
+    protected $model = Stadium::class
+      
+    
     /**
      * Define the model's default state.
      *
@@ -13,8 +23,10 @@ class StadiumFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
+        return [ 
+            'name' => $this->faker->name(),
+            'capacity' => $this->faker->random(),
+            
         ];
     }
 }
